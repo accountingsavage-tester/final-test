69fbbb9d636c5300e5730c1e452619b6e42378ab
accountingsavage-tester <forclaudemails@gmail.com>
2026-08-30 21:27:35 +0800

    Add working calculator and lyric timing logic

---
+++ app.js
@@ -0,0 +1,8 @@
+const d=document.getElementById('d'),a=document.getElementById('a'),l=document.getElementById('l'),t=document.getElementById('t');
+const words=`Nais kong magpakalasing
+Dahil wala ka na
+
+Nakatingin sa salamin
+At nag-iisa
+
+Nakatanim pa
